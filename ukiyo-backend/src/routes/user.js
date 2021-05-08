1667a6567ea41e73a4502265fb00ddc6b2df4c7b
Michael Lam <mlam11@uoguelph.ca>
2021-05-07 21:59:26 -0400

    Get user endpoint

--- ukiyo-backend/src/routes/user.js
+++ ukiyo-backend/src/routes/user.js
@@ -13,7 +13,6 @@ const router = express.Router();
 // Create user
 router.post('/', async (req, res, next) => {
     const { first_name, last_name, nickname, email, password } = req.body;
-    console.log(req.body);
     try {
         // Create user in Firebase, Firebase auth will check if user exists
         const response = await auth.createUserWithEmailAndPassword(email, password);
@@ -22,8 +21,22 @@ router.post('/', async (req, res, next) => {
         await User.create({ uid, first_name, last_name, nickname, email, password });
         res.status(200).json({ user: { uid, first_name, last_name, nickname, email } });
     } catch (error) {
-        res.status(412);
-        res.json(error.message);
+        res.status(412).json(error.message);
+    }
+})
+
+// Get user
+router.get('/:id', async (req, res, next) => {
+    const uid = req.params.id;
+    try {
+        const user = await User.findOne({ uid });
+        if (user) {
+            const { first_name, last_name, nickname, email } = user;
+            return res.status(200).json({ user: { uid, first_name, last_name, nickname, email } })
+        }
+        res.status(404).json('User not found');
+    } catch (error) {
+        res.status(400).json(error.message);
     }
 })
 
